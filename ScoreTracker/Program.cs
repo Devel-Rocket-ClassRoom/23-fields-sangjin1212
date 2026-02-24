53f6c4a53bb7f572c8639f37c626a326be2817ac
sangjin1212 <sangjin411@gmail.com>
2026-02-24 20:05:06 +0900

    윤상진
    ScoreTracKer 완료

--- ScoreTracker/Program.cs
+++ ScoreTracker/Program.cs
@@ -1,4 +1,56 @@
 using System;
 
-// README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+ScoreTracker math = new ScoreTracker("수학");
+math.ShowScore();
+Console.WriteLine();
+
+math.SetScore(85);
+math.AddBonus(10);
+math.AddBonus(20);
+math.SetScore(-10);
+Console.WriteLine();
+math.ShowScore();
+
+class ScoreTracker
+{
+    const int MaxScore = 100;
+    const int MinScore = 0;
+    public readonly string name;
+    private int score;
+    private int BonusCount;
+    public ScoreTracker(string name)
+    {
+        this.name = name;
+    }
+
+    public void SetScore(int score)
+    {
+        if (score < MinScore || score > MaxScore)
+        {
+            Console.WriteLine("점수는 0~100 사이여야 합니다.");
+            return;
+        }
+        else
+        {
+            this.score = score;
+            Console.WriteLine($"점수를 {score}점으로 설정 했습니다.");
+        }
+    }
+    public void AddBonus(int bonus)
+    {
+        score += bonus;
+        BonusCount++;
+        if (score > 100)
+        {
+            score = MaxScore;
+        }
+        Console.WriteLine($"{bonus}점 보너스 적용! 현재 점수: {score}");
+
+    }
+    public void ShowScore()
+    {
+        Console.WriteLine($"=== {name} ===");
+        Console.WriteLine($"점수: {score} / {MaxScore}");
+        Console.WriteLine($"보너스 적용 횟수: {BonusCount}");
+    }
+}
